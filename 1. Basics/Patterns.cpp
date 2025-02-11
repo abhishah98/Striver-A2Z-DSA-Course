177d88c8153bdcc8f283a9355361c971362c7769
Abhishek Shah <abhishek.shah.contractor@pepsico.com>
2025-02-11 12:25:53 +0530

    Added Pattern solution for 12 to 17

--- 1. Basics/Patterns.cpp
+++ 1. Basics/Patterns.cpp
@@ -9,6 +9,85 @@ int main() {
     int n = 5;
     cout << "\n\nOutput:" << endl;
 
+    
+/*    
+    // Pattern 17
+    char cnt = 'A';
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n-i-1; j++) {
+            cout<< " ";
+        }
+        cnt = 'A';
+        int mid = (2*i+1)/2;
+        for(int j=1;j<=2*i+1;j++){
+            cout << cnt;
+            if (j<=mid) cnt++;
+            else cnt--;
+        }
+        cout << endl;
+    }
+*/
+
+/*    
+    // Pattern 16
+    char cnt = 'A';
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<=i; j++) {
+            cout<<cnt << " ";
+        }
+        cnt++;
+        cout << endl;
+    }            
+*/
+/*
+    // Pattern 15
+    char cnt = 'A';
+    for (int i=0; i<n; i++) {
+        cnt = 'A';
+        for (int j=0; j<(n-i); j++) {
+            cout<<cnt << " ";
+            cnt++;
+        }
+        cout << endl;
+    }        
+*/
+/*
+    // Pattern 14
+    char cnt = 'A';
+    for (int i=0; i<n; i++) {
+        cnt = 'A';
+        for (int j=0; j<=i; j++) {
+            cout<<cnt << " ";
+            cnt++;
+        }
+        cout << endl;
+    }    
+/*    
+    
+/*    
+    // Pattern 13
+    int cnt = 1;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<=i; j++) {
+            cout<<cnt << " ";
+            cnt++;
+        }
+        cout << endl;
+    }
+*/    
+/*    
+    // Pattern 12
+    for(int i=1; i<=n; i++) {
+        for (int j=1; j<=i; j++)
+            cout << j;
+        for (int j=1; j<=(n-i)*2; j++)
+            cout << " ";
+        for (int j=i; j>=1; j--) 
+            cout << j;
+        cout << endl;
+    }
+*/
+
 /*
     // Pattern 11
     for (int i=0; i<n; i++) {
